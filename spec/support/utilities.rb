dfd105f7620c589896e542d4f8a40de69ac6d070
Jonathan Smith <fullmetalultimaknight@gmail.com>
2014-05-16 22:10:21 -0500

    Finish layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+#full-title helper for RSpec
+def full_title(page_title)
+	base_title = "Ruby on Rails Tutorial Sample App"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
